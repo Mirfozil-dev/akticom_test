092df53f951de786f05a109f62ccb39ed73a8918
Robenson Jack <robensonjack@Robensons-MacBook-Pro.local>
2024-01-22 15:40:28 +0200

    animation fixes

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { useClientsStore } from '@/stores/clients';
 import { useRoute } from 'vue-router' 
 import { GET_CLIENT_BY_ID } from '@/api'
 import { onMounted, ref } from 'vue';
@@ -20,19 +19,21 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div v-if="isLoading" class="loader">
-    <img
-      src="@/assets/images/Rocket.gif"
-    />
-  </div>
-  <div class="client_card" v-else>
-    <h1>{{ client.fullname }}</h1>
-    <p>Информация о клиенте</p>
-    <p>Номер телефона: {{ client.phone }}</p>
-    <p>Регион: {{ client.region }}</p>
-    <p>Статус: {{ client.status }}</p>
-    <p>Дата создания: {{ formatDate(client.created_at) }}</p>
-  </div>
+  <Transition name="fade">
+    <div v-if="isLoading" class="loader">
+      <img
+        src="@/assets/images/Rocket.gif"
+      />
+    </div>
+    <div class="client_card" v-else>
+      <h1>{{ client.fullname }}</h1>
+      <p>Информация о клиенте</p>
+      <p>Номер телефона: {{ client.phone }}</p>
+      <p>Регион: {{ client.region }}</p>
+      <p>Статус: {{ client.status }}</p>
+      <p>Дата создания: {{ formatDate(client.created_at) }}</p>
+    </div>
+  </Transition>
 </template>
 
 <style scoped lang="scss">
@@ -41,5 +42,16 @@ onMounted(async () => {
   border-radius: 6px;
   padding: 10px 20px;
   margin: 20px 10px;
+}
+
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .4s
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0
 }
 </style>
